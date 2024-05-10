bc7c73b758a17caee10c2a24d8f82093ed86b9fd
AliHMohammad <alihmohammad@hotmail.com>
2024-05-10 10:13:31 +0200

    feat: created added sorting to product pagination

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -7,6 +7,7 @@ import DataTable, { PaginationSize } from "@/components/core/DataTable";
 import { ProductColumns } from "@/components/products/ProductColumns";
 import { Button } from "@/components/ui/button";
 import { IPagination } from "@/models/IPagination";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 
 
@@ -16,6 +17,10 @@ export default function ProductsPage() {
 		pageIndex: 0, //initial page index
 		pageSize: 5, //default page size
 	});
+	const [sort, setSort] = useState({
+		sortBy: "id",
+		sortDir: "ASC"
+	});
 
 	
 
@@ -23,6 +28,7 @@ export default function ProductsPage() {
 		const queryParams = new URLSearchParams({
 			pageIndex: String(pagination.pageIndex),
 			pageSize: String(pagination.pageSize),
+			...sort
 		}).toString();
 
 		console.log(queryParams);
@@ -39,27 +45,50 @@ export default function ProductsPage() {
 					variant: "destructive",
 				});
 			});
-	}, [pagination]);
+	}, [pagination, sort]);
 
 	return (
 		<>
-			<Link to={"form"}>
-				<Button>Tilføj produkt</Button>
-			</Link>
+			<div className="flex justify-between">
+				<div className="flex gap-2">
+					
+					<Select onValueChange={(value) => setSort((prevState) => ({ ...prevState, sortBy: value }))}>
+						<SelectTrigger className="w-[180px]">
+							<SelectValue placeholder="Sorter efter" />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value="id">ID</SelectItem>
+							<SelectItem value="name">Navn</SelectItem>
+							<SelectItem value="price">Pris</SelectItem>
+							<SelectItem value="stock">Antal</SelectItem>
+						</SelectContent>
+					</Select>
+
+					<Select defaultValue="ASC" onValueChange={(value) => setSort((prevState) => ({ ...prevState, sortDir: value }))}>
+						<SelectTrigger className="w-[120px]">
+							<SelectValue/>
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value="ASC">Ascending</SelectItem>
+							<SelectItem value="DESC">Descending</SelectItem>
+						</SelectContent>
+					</Select>
+				</div>
+				<Link to={"form"}>
+					<Button>Tilføj produkt</Button>
+				</Link>
+				
+			</div>
 			{products && <DataTable columns={ProductColumns} data={products.content} pagination={pagination} />}
 
 			<div className="flex justify-evenly">
-				<Button
-					onClick={() => setPagination((prevState) => ({ ...prevState, pageIndex: prevState.pageIndex - 1 }))}
-					disabled={products?.first}
-				>
+				<Button onClick={() => setPagination((prevState) => ({ ...prevState, pageIndex: prevState.pageIndex - 1 }))} disabled={products?.first}>
 					{"Forrige"}
 				</Button>
-				<p>Side {pagination.pageIndex + 1} / {products?.totalPages}</p>
-				<Button
-					onClick={() => setPagination((prevState) => ({ ...prevState, pageIndex: prevState.pageIndex + 1 }))}
-					disabled={products?.last}
-				>
+				<p>
+					Side {pagination.pageIndex + 1} / {products?.totalPages}
+				</p>
+				<Button onClick={() => setPagination((prevState) => ({ ...prevState, pageIndex: prevState.pageIndex + 1 }))} disabled={products?.last}>
 					{"Næste"}
 				</Button>
 			</div>

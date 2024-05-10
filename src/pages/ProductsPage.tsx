3cfb229e814dcde6e36294c787c3f2a5176391bd
AliHMohammad <alihmohammad@hotmail.com>
2024-05-10 09:43:00 +0200

    feat: added frontend basic pagination to products

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -3,21 +3,34 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllProducts } from "@/services/productApi";
 import { toast } from "@/components/ui/use-toast";
-import DataTable from "@/components/core/DataTable";
+import DataTable, { PaginationSize } from "@/components/core/DataTable";
 import { ProductColumns } from "@/components/products/ProductColumns";
 import { Button } from "@/components/ui/button";
+import { IPagination } from "@/models/IPagination";
 
 
 
 export default function ProductsPage() {
-	const [products, setProducts] = useState<IProduct[] | null>(null);
+	const [products, setProducts] = useState<IPagination<IProduct> | null>(null)
+	const [pagination, setPagination] = useState<PaginationSize>({
+		pageIndex: 0, //initial page index
+		pageSize: 5, //default page size
+	});
 
-	console.log(products);
+	
 
 	useEffect(() => {
-		getAllProducts()
+		const queryParams = new URLSearchParams({
+			pageIndex: String(pagination.pageIndex),
+			pageSize: String(pagination.pageSize),
+		}).toString();
+
+		console.log(queryParams);
+		
+
+		getAllProducts(queryParams)
 			.then(({data}) => {
-				setProducts(data.content);
+				setProducts(data);
 			})
 			.catch(() => {
 				toast({
@@ -26,14 +39,30 @@ export default function ProductsPage() {
 					variant: "destructive",
 				});
 			});
-	}, []);
+	}, [pagination]);
 
 	return (
 		<>
 			<Link to={"form"}>
 				<Button>Tilføj produkt</Button>
 			</Link>
-			{products && <DataTable columns={ProductColumns} data={products} />}
+			{products && <DataTable columns={ProductColumns} data={products.content} pagination={pagination} />}
+
+			<div className="flex justify-evenly">
+				<Button
+					onClick={() => setPagination((prevState) => ({ ...prevState, pageIndex: prevState.pageIndex - 1 }))}
+					disabled={products?.first}
+				>
+					{"Forrige"}
+				</Button>
+				<p>Side {pagination.pageIndex + 1} / {products?.totalPages}</p>
+				<Button
+					onClick={() => setPagination((prevState) => ({ ...prevState, pageIndex: prevState.pageIndex + 1 }))}
+					disabled={products?.last}
+				>
+					{"Næste"}
+				</Button>
+			</div>
 		</>
 	);
 }

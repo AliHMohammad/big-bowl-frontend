af21a03a52fa8d758fab3d5db3e753df89ced37c
Forkeh <forkeh@gmail.com>
2024-05-16 13:21:32 +0200

    feat: added videoplayer on homepage

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,19 +1,16 @@
 import { ReactNode } from "react";
-import NavBar from "@/components/layouts/NavBar.tsx";
 import Header from "@/components/layouts/Header.tsx";
 
 type Props = {
-	children: ReactNode
-}
+	children: ReactNode;
+};
 
-export default function PageLayout({children}: Props) {
+export default function PageLayout({ children }: Props) {
 	return (
 		<>
-			<Header/>
-			
-			<main className={"m-8"}>
-				{children}
-			</main>
+			<Header />
+
+			<main>{children}</main>
 		</>
 	);
 }

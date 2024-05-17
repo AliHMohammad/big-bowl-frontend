e8441f94d7bea45908806a1ead55cd2032532f49
Forkeh <forkeh@gmail.com>
2024-05-17 11:18:55 +0200

    fix: homepage carousel resizes on small screens

--- src/components/homepage/ActivityCarousel.tsx
+++ src/components/homepage/ActivityCarousel.tsx
@@ -1,12 +1,12 @@
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
-import ActivityCarouselItem from "./ActivityCarouselItem";
 import Autoplay from "embla-carousel-autoplay";
+import ActivityCarouselItem from "./ActivityCarouselItem";
 
 export default function ActivityCarousel() {
 	return (
-		<section className="w-[52rem]">
+		<section className="w-[16.5rem] sm:w-[52rem]">
 			<Carousel
-				className="h-[35rem] flex items-center"
+				className="h-[26rem] flex items-center"
 				opts={{ loop: true }}
 				plugins={[
 					Autoplay({
@@ -15,19 +15,19 @@ export default function ActivityCarousel() {
 				]}
 			>
 				<CarouselContent>
-					<CarouselItem className="basis-1/3">
+					<CarouselItem className="sm:basis-1/3 ">
 						<ActivityCarouselItem image="xp-bowl-1.jpg" text="Spisning" color="bg-orange-500" />
 					</CarouselItem>
-					<CarouselItem className="basis-1/3">
+					<CarouselItem className="sm:basis-1/3">
 						<ActivityCarouselItem image="xp-bowl-2.jpg" text="Klar til strike" color="bg-green-500" />
 					</CarouselItem>
-					<CarouselItem className="basis-1/3">
+					<CarouselItem className="sm:basis-1/3">
 						<ActivityCarouselItem image="xp-bowl-3.jpg" text="Så der kamp!" color="bg-yellow-500" />
 					</CarouselItem>
-					<CarouselItem className="basis-1/3">
+					<CarouselItem className="sm:basis-1/3">
 						<ActivityCarouselItem image="xp-bowl-4.jpg" text="Familietid" color="bg-blue-500" />
 					</CarouselItem>
-					<CarouselItem className="basis-1/3">
+					<CarouselItem className="sm:basis-1/3">
 						<ActivityCarouselItem image="xp-bowl-5.jpg" text="Hygge og sjov" color="bg-red-500" />
 					</CarouselItem>
 				</CarouselContent>

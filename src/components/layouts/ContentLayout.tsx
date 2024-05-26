1d592b3181d6c1ba23f3271e2916369463528d5f
Forkeh <forkeh@gmail.com>
2024-05-26 19:06:15 +0200

    style: added max width to contentLayout

--- src/components/layouts/ContentLayout.tsx
+++ src/components/layouts/ContentLayout.tsx
@@ -12,7 +12,7 @@ export default function ContentLayout({ children }: Props) {
 
 	return (
 		<motion.div key={location.pathname}
-					className="m-8"
+					className="m-8 mx-auto max-w-screen-lg"
 					initial={{
 						opacity: 0,
 					}}

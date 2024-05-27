265c355c6188d7f76932503b2ab7c26e061edbf1
AliHMohammad <alihmohammad@hotmail.com>
2024-05-27 09:50:23 +0200

    fix: fixed spacing in VideoPlayer.tsx

--- src/components/homepage/VideoPlayer.tsx
+++ src/components/homepage/VideoPlayer.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 
-
 export default function VideoPlayer() {
 	return (
 		<section className="relative flex justify-center h-[400px] sm:h-[600px] overflow-hidden">
@@ -13,17 +12,16 @@ export default function VideoPlayer() {
 			></video>
 			<div className=" z-50 relative flex items-center justify-center w-2/3 sm:w-1/3 text-center m-auto">
 				<motion.p className="text-white text-3xl sm:text-6xl font-bold">
-					<motion.span className="text-orange-300"
-								 initial={{ opacity: 0, x: -100 }}
-								 animate={{ opacity: 1, x: 0 }}
-								 transition={{ delay: 3, duration: 1 }}
-					>De sjoveste oplevelser
-					</motion.span>
 					<motion.span
-						initial={{ opacity: 0, x: 100 }}
+						className="text-orange-300"
+						initial={{ opacity: 0, x: -100 }}
 						animate={{ opacity: 1, x: 0 }}
-						transition={{ delay: 4, duration: 1 }}
-					>&nbsp;er dem, vi skaber med hinanden
+						transition={{ delay: 3, duration: 1 }}
+					>
+						De sjoveste oplevelser
+					</motion.span>
+					<motion.span initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 4, duration: 1 }}>
+						&nbsp;er dem, vi skaber med hinanden
 					</motion.span>
 				</motion.p>
 			</div>
